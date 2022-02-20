0e9fa22df4aaa1a84039aed0f727a4950d07d706
lham-ss <lham.staffsetter@gmail.com>
2022-02-20 12:40:55 -0700

    move middleware in admin routes

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -5,13 +5,13 @@ module.exports = app => {
 
     var router = require('express').Router();
 
-    router.post("/", isUserAuthenticated, admin.create);                // create a new admin document in mongo
-    router.get("/", isUserAuthenticated, admin.findAll);                // list all admins
-    router.get("/active", isUserAuthenticated, admin.findAllActive);    // list all active admins
-    router.get("/:id", isUserAuthenticated, admin.findOne);             // find admin by id
-    router.put("/:id", isUserAuthenticated, admin.update);              // update admin by id
-    router.delete("/:id", isUserAuthenticated, admin.delete);           // delete admin by id
-    router.delete("/", isUserAuthenticated, admin.deleteAll);           // delete everything
+    router.post("/", admin.create);                // create a new admin document in mongo
+    router.get("/", admin.findAll);                // list all admins
+    router.get("/active", admin.findAllActive);    // list all active admins
+    router.get("/:id", admin.findOne);             // find admin by id
+    router.put("/:id", admin.update);              // update admin by id
+    router.delete("/:id", admin.delete);           // delete admin by id
+    router.delete("/", admin.deleteAll);           // delete everything
 
-    app.use('/api/admin', router);
+    app.use('/api/admin', isUserAuthenticated, router);
 }
